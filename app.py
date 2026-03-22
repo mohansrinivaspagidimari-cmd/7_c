f682b9f0c46ccccd6f2ee437d483df05de86ed62
Mohan Pagidimari <mohansrinivaspagidimari@gmail.com>
2026-03-22 20:14:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Render is working!"
 
 if __name__ == "__main__":
-    print(add(2, 3))
+    app.run(host="0.0.0.0", port=10000)
